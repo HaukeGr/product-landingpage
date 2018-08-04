bb5e0842fff855f52f8bd8971cf57e8e35fe6fc7
Hauke Grothues <hauke.grothues@gmail.com>
2018-08-04 23:07:51 +0200

    Add learn more scroll

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function() {
+  $(".scroll-text").click(function() {
+    var offset = 20;
+    $('html, body').animate({
+        scrollTop: $("#internet-sec").offset().top + offset
+    }, 500);
+});
+});
