16e78ce6ff1cad01322f3e385fb15f40f9b2fae2
Hauke Grothues <hauke.grothues@web.de>
2018-08-05 12:07:46 +0200

    complete all 16 user stories

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
-  $(".scroll-text").click(function() {
-    var offset = 20;
+  var offset = 20;
+  $(".nav-link").click(function() {
+    var scrollTo;
+    switch($(this).text()) {
+      case "the new internet":
+        scrollTo = "#internet-sec";
+        break;
+      case "the tech":
+        scrollTo = "#tech-sec";
+        break;
+      case "contact":
+        scrollTo = "#contact-sec";
+        break;
+    }
+    $('html, body').animate({
+        scrollTop: $(scrollTo).offset().top + offset
+    }, 500);
+  });
+
+  $(".scroll-div").click(function() {
     $('html, body').animate({
         scrollTop: $("#internet-sec").offset().top + offset
     }, 500);
